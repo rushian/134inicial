0df706726660e6aba7fad875fa04f5fe58dbb61b
rushian_san <luciano.ivec@gmail.com>
2022-05-23 01:02:37 -0300

    Para a entidade Pet, criacao do metodo Delete e do metodo Post com montagem de json dinamico a partir de um arquivo csv

--- main.py
+++ main.py
@@ -1,6 +1,63 @@
 import pytest
 
 
+def teste_aprendizado():
+    json_tag = '"tags": ['
+    lista = [['71', 'vacinado'], ['72', 'pequeno porte'], ['73', 'castrado']]
+    itens_lista = len(lista)
+    print(f'\nExistem  {itens_lista} tags na {lista}')
+    for contador in range(1, itens_lista):
+        if contador != itens_lista:
+            json_tag += '{"id":' + lista[contador][0] + ',"name":"' + lista[contador][1] + '"},'
+        else:
+            json_tag += '{"id":' + lista[contador][0] + '"name":"' + lista[contador][1] + '"}'
+    json_tag += ']'
+    print(json_tag)
+
+
+def teste_aprendizado1():
+    lista_tags = "73,castrado"
+    #lista_tags = "71, vacinado;72,pequeno porte;73,castrado"
+
+    #lista_tags = lista_tags.split(';')
+    json_tag1 = '"tags": ['
+    sub_lista_tags = []
+    if lista_tags.count(';') > 0:
+        lista_tags = lista_tags.split(';')
+        qtd_el = len(lista_tags)
+        print('Quantidade de elementos' + str(qtd_el))
+
+        for i in range(0, qtd_el):
+            # Create an index range for l of n items:
+            sub_lista_tags.append(lista_tags[i].split(','))
+        itens_lista1 = len(sub_lista_tags)
+        print(f'\nExistem  {itens_lista1} tags na lista {sub_lista_tags}')
+        for contador1 in range(0, itens_lista1):
+            if contador1 < itens_lista1 - 1:
+                json_tag1 += '{"id":' + sub_lista_tags[contador1][0] + ',"name":"' + sub_lista_tags[contador1][1] + '"},'
+            else:
+                json_tag1 += '{"id":' + sub_lista_tags[contador1][0] + ',"name":"' + sub_lista_tags[contador1][1] + '"}'
+    else:
+        lista_tags = lista_tags.split(',')
+        json_tag1 += '{"id":' + lista_tags[0] + ',"name":"' + lista_tags[1] + '"}'
+
+    json_tag1 += ']'
+    print('\n', json_tag1)
+
+
+
+def teste_create_sublists_alt():
+    big_list = "71, vacinado;72,pequeno porte;73,castrado".split(';')
+    qty_el = len(big_list)
+    sub_lista_tags = []
+    for i in range(0, qty_el):
+        # Create an index range for l of n items:
+        sub_lista_tags.append(big_list[i].split(','))
+
+
+    print(f'\n {sub_lista_tags}')
+
+
 def imprimir_oi(nome):
     print(f'Olá, {nome}. Bem vindo ao python.')
 
--- tests/api/test_pet.py
+++ tests/api/test_pet.py
@@ -1,9 +1,11 @@
 # bibliotecas
 import json
+import os
 
 import pytest
 import requests
 from tests.utils.file_manager import ler_csv
+
 # variaveis publicas
 url = 'https://petstore.swagger.io/v2/pet'
 headers = {'Content-Type': 'application/json'}
@@ -25,7 +27,7 @@ def teste_incluir_pet():
 
         url=url,
         headers=headers,
-        data=open('E:\\dev\\pyCharm\\134inicial\\vendors\\json\\pet1.json')
+        data=open('/vendors\\json\\pet1.json')
     )
 
     # valida
@@ -87,13 +89,12 @@ def teste_alterar_pet():
     resultado_obtido = requests.put(
         url=url,
         headers=headers,
-        data=open('E:\\dev\\pyCharm\\134inicial\\vendors\\json\\pet2.json')
+        data=open('/vendors\\json\\pet2.json')
     )
 
     # valida
     print(f'\nResultado obtido: ', resultado_obtido)
     corpo_do_resultado_obtido = resultado_obtido.json()
-    # print(f'\nCorpo do resultado obtido: \n', corpo_do_resultado_obtido)
     print(f'\nCorpo do resultado obtido: \n', json.dumps(corpo_do_resultado_obtido, indent=3))
 
     assert resultado_obtido.status_code == status_code_esperado
@@ -103,8 +104,9 @@ def teste_alterar_pet():
     assert corpo_do_resultado_obtido['tags'][0]['name'] == pet_tag_esperada
     assert corpo_do_resultado_obtido['status'] == pet_status_esperado
 
+
 def teste_excluir_pet():
-    #configura
+    # configura
     #   dados de entrada
     pet_id = 777305
     #   resultado esperado
@@ -112,14 +114,14 @@ def teste_excluir_pet():
     tipo_esperado = 'unknown'
     messagem_esperada = '777305'
 
-    #executa
+    # executa
     resultado_obtido = requests.delete(
         url=url + '/' + str(pet_id),
         headers=headers
 
     )
 
-    #valida
+    # valida
     print(f'\nResultado obtido: ', resultado_obtido)
     corpo_do_resultado_obtido = resultado_obtido.json()
     # print(f'\nCorpo do resultado obtido: \n', corpo_do_resultado_obtido)
@@ -128,54 +130,50 @@ def teste_excluir_pet():
     assert corpo_do_resultado_obtido['type'] == tipo_esperado
     assert corpo_do_resultado_obtido['message'] == messagem_esperada
 
+
 @pytest.mark.parametrize('pet_id,category_id,category_name,pet_name,tags_id,tags_name,status',
                          ler_csv('E:\\dev\\pyCharm\\134inicial\\vendors\\csv\\massa_incluir_pet.csv'))
-
-def teste_incluir_pet_em_massa(pet_id,category_id,category_name,pet_name,tags_id,tags_name,status):
+def teste_incluir_pet_em_massa(pet_id, category_id, category_name, pet_name, tags_id, tags_name, status):
     # 1. Configura
     # 1.1 Dados de entrada
     # Os dados de entrada proveem do arquivo massa_incluir_pets.csv
     # 1.1.1 Montagem do JSON dinamico
-    corpo_json =  '{'
-    corpo_json += f'"id":  "{ pet_id }" ,'
+    corpo_json = '{'
+    corpo_json += f'"id":  "{pet_id}" ,'
     corpo_json += '"category": {'
-    corpo_json += f'"id":  "{ category_id }",'
-    corpo_json += f'"name": "{ category_name }"'
+    corpo_json += f'"id":  "{category_id}",'
+    corpo_json += f'"name": "{category_name}"'
     corpo_json += ' },'
-    corpo_json += f'"name":  "{ pet_name}" ,'
+    corpo_json += f'"name":  "{pet_name}" ,'
     corpo_json += '"photoUrls": ['
     corpo_json += '"string"'
     corpo_json += '],'
     corpo_json += '"tags": ['
     corpo_json += '{'
-    corpo_json += f'"id":  "{tags_id }",'
+    corpo_json += f'"id":  "{tags_id}",'
     corpo_json += f'"name":  "{tags_name}"'
     corpo_json += '}'
     corpo_json += '],'
-    corpo_json += f'"status":  "{ status}"'
+    corpo_json += f'"status":  "{status}"'
     corpo_json += '}'
-
 
     # 1.2 Resultados Esperados
     # Os dados de entrada também servirão como resultados
     # esperados, visto que o retorno é um eco
     status_code_esperado = 200
 
-
-    #Executa
+    # Executa
     resultado_obtido = requests.post(
         url=url,
         headers=headers,
         data=corpo_json
     )
 
+    # Valida
 
-    #Valida
-
-    print(f'\nCORPO: ', corpo_json)
+    print(f'\nCORPO: \n', json.dumps(corpo_json, indent=3))
     print(f'\nResultado obtido: ', resultado_obtido)
     corpo_do_resultado_obtido = resultado_obtido.json()
-    # print(f'\nCorpo do resultado obtido: \n', corpo_do_resultado_obtido)
     print(f'\nCorpo do resultado obtido: \n', json.dumps(corpo_do_resultado_obtido, indent=3))
     assert resultado_obtido.status_code == status_code_esperado
     assert corpo_do_resultado_obtido['id'] == int(pet_id)
@@ -184,4 +182,92 @@ def teste_incluir_pet_em_massa(pet_id,category_id,category_name,pet_name,tags_id
     assert corpo_do_resultado_obtido['tags'][0]['name'] == tags_name
 
 
+os.chdir(f'E:{os.sep}dev{os.sep}pyCharm{os.sep}134inicial{os.sep}')
+
+
+@pytest.mark.parametrize('pet_id,category_id,category_name,pet_name,tags,status',
+                         ler_csv(f'vendors{os.sep}csv{os.sep}massa_incluir_pet_multitags.csv'))
+def teste_incluir_pet_em_massa_com_multiplas_tags(pet_id, category_id, category_name, pet_name, tags, status):
+    # 1. Configura
+    # 1.1 Dados de entrada
+    # Os dados de entrada proveem do arquivo massa_incluir_pets.csv
+    # 1.1.1 Montagem do JSON dinamico
+    global itens_lista
+    corpo_json = '{'
+    corpo_json += f'"id":  "{pet_id}" ,'
+    corpo_json += '"category": {'
+    corpo_json += f'"id":  "{category_id}",'
+    corpo_json += f'"name": "{category_name}"'
+    corpo_json += ' },'
+    corpo_json += f'"name":  "{pet_name}" ,'
+    corpo_json += '"photoUrls": ['
+    corpo_json += '"string"'
+    corpo_json += '],'
+    #1.1.1.1 iteração das possiveis tags doo animal
+    lista_tags = tags
+    json_tag = '"tags": ['
+    sub_lista_tags = []
+    qtd_tags = lista_tags.count(';') + 1
+    # quando for mais de uma tag, os valores devem ser separados no csv por ';'
+    if lista_tags.count(';') > 0:
+        lista_tags = lista_tags.split(';')
+        qtd_el = len(lista_tags)
+        print('Quantidade de elementos' + str(qtd_el))
+
+        for i in range(0, qtd_el):
+            # Create an index range for l of n items:
+            sub_lista_tags.append(lista_tags[i].split(','))
+        itens_lista = len(sub_lista_tags)
+        #print(f'\nExistem  {itens_lista} tags na lista {sub_lista_tags}')
+        for contador in range(0, itens_lista):
+            if contador < itens_lista - 1:
+                json_tag += '{"id":' + sub_lista_tags[contador][0] + ',"name":"' + sub_lista_tags[contador][1] + '"},'
+            else:
+                json_tag += '{"id":' + sub_lista_tags[contador][0] + ',"name":"' + sub_lista_tags[contador][1] + '"}'
+    # 1.1.1.2 Quando há somente 1 tag, não é necessário usar o looping do FOR
+    else:
+        lista_tags = lista_tags.split(',')
+        json_tag += '{"id":' + lista_tags[0] + ',"name":"' + lista_tags[1] + '"}'
+        #print(f'\nExiste 1 tag na lista {lista_tags}')
+    json_tag += '],'
+    # 1.1.2 Continuando a montagem do Json, com adicao das tags
+    corpo_json += json_tag
+    corpo_json += f'"status":  "{status}"'
+    corpo_json += '}'
+    # print(corpo_json)
+
+    # 1.2 Resultados Esperados
+    # Os dados de entrada também servirão como resultados
+    # esperados, visto que o retorno é um eco
+    status_code_esperado = 200
+
+    # Executa
+    resultado_obtido = requests.post(
+        url=url,
+        headers=headers,
+        data=corpo_json
+    )
+
+    # Valida
+    mostrar_corpo_json = json.loads(corpo_json)
+    print(f'\n==== CORPO ENVIADO ====')
+    print(json.dumps(mostrar_corpo_json, indent=4))
+
+    print(f'\n==== CORPO OBTIDO ====')
+    corpo_do_resultado_obtido = resultado_obtido.json()
+    print(json.dumps(corpo_do_resultado_obtido, indent=4))
+
+    assert resultado_obtido.status_code == status_code_esperado
+    assert corpo_do_resultado_obtido['id'] == int(pet_id)
+    assert corpo_do_resultado_obtido['name'] == pet_name
+    assert corpo_do_resultado_obtido['category']['name'] == category_name
+    # asserts dinamicos de acordo com as tags existeentes
+    if qtd_tags > 1:
+        #print(f'\nExistem  {itens_lista} tags na lista {sub_lista_tags}')
+        for i in range(0, qtd_tags):
+            for j in range(2):
+                assert corpo_do_resultado_obtido['tags'][i]['name'] == sub_lista_tags[i][1]
+    else:
+        #print(f'\nExiste 1 tag na lista {lista_tags}')
+        assert corpo_do_resultado_obtido['tags'][0]['name'] == lista_tags[1]
 
